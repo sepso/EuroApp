29b49efdffef4232f858078b87f81598e7f64a38
unknown <test@ecv-vpn.ECV-VPN>
2018-02-07 13:31:20 -0600

    SePsO

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -33,7 +33,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="http://www.creative-tim.com">Creative Tim</a>
+                <a class="navbar-brand" href="#">{{ config('app.name', 'Laravel') }}</a>
             </div>
 
             <div class="collapse navbar-collapse" id="navigation-example">
@@ -69,16 +69,17 @@
     </nav>
 
     <div class="wrapper">
-        <div class="header header-filter" style="background-image: url('https://images.unsplash.com/photo-1423655156442-ccc11daa4e99?crop=entropy&dpr=2&fit=crop&fm=jpg&h=750&ixjsv=2.1.0&ixlib=rb-0.3.5&q=50&w=1450');">
+        {{-- <div class="header header-filter" style="background-image: url('https://images.unsplash.com/photo-1423655156442-ccc11daa4e99?crop=entropy&dpr=2&fit=crop&fm=jpg&h=750&ixjsv=2.1.0&ixlib=rb-0.3.5&q=50&w=1450');"> --}}
+         <div class="header header-filter" style="background-image: url('https://images.unsplash.com/photo-1503797172624-decbe212fdb5?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=84afaa3ec26e5fcdf7ac81a8b496418a&auto=format&fit=crop&w=1353&q=80');">
             <div class="container">
                 <div class="row">
                     <div class="col-md-6">
-                        <h1 class="title">Write the best title for your page.</h1>
-                        <h4>Every landing page needs a small description after the big bold title, that's why we added this text here. Add here all the information that can make you or your product create the first impression.</h4>
-                        <br />
-                        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="btn btn-danger btn-raised btn-lg">
+                        <h1 class="title">Para poder ir en búsqueda de lo mejor.</h1>
+                        <h4>El mercado de las aplicaciones cada vez está creciendo más, y para las grandes empresas estas han funcionado como un motor de crecimiento y de eficiencia.</h4>
+                        {{-- <br /> --}}
+                        {{-- <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="btn btn-danger btn-raised btn-lg">
                             <i class="fa fa-play"></i> Watch video
-                        </a>
+                        </a> --}}
                     </div>
                 </div>
             </div>
@@ -86,10 +87,10 @@
 
         <div class="main main-raised">
             <div class="container">
-                <div class="section text-center section-landing">
+                {{-- <div class="section text-center section-landing">
                     <div class="row">
                         <div class="col-md-8 col-md-offset-2">
-                            <h2 class="title">Let's talk product</h2>
+                            <h2 class="title">EuroTeam</h2>
                             <h5 class="description">This is the paragraph where you can write more details about your product. Keep you user engaged by providing meaningful information. Remember that by this time, the user is curious, otherwise he wouldn't scroll to get here. Add a button if you want the user to see more.</h5>
                         </div>
                     </div>
@@ -125,91 +126,87 @@
                             </div>
                         </div>
                     </div>
-                </div>
+                </div> --}}
 
                 <div class="section text-center">
-                    <h2 class="title">Here is our team</h2>
-
+                    <h2 class="title">Ellos son nuestro EuroTeam</h2>
+    
                     <div class="team">
                         <div class="row">
                             <div class="col-md-4">
                                 <div class="team-player">
-                                    <img src="{{ asset('/MK_BS3/img/avatar.jpg') }}" alt="Thumbnail Image" class="img-raised img-circle">
-                                    <h4 class="title">Gigi Hadid <br />
-                                        <small class="text-muted">Model</small>
+                                    <img src="{{ asset('/img/JuanPadilla.jpg') }}" alt="Thumbnail Image" class="img-raised img-circle">
+                                    <h4 class="title">Juan Padilla<br />
+                                        <small class="text-muted">Creative designer</small>
                                     </h4>
-                                    <p class="description">You can write here details about one of your team members. You can give more details about what they do. Feel free to add some <a href="#">links</a> for people to be able to follow them outside the site.</p>
+                                    {{-- <p class="description">You can write here details about one of your team members. You can give more details about what they do. Feel free to add some <a href="#">links</a> for people to be able to follow them outside the site.</p>
                                     <a href="#pablo" class="btn btn-simple btn-just-icon"><i class="fa fa-twitter"></i></a>
                                     <a href="#pablo" class="btn btn-simple btn-just-icon"><i class="fa fa-instagram"></i></a>
-                                    <a href="#pablo" class="btn btn-simple btn-just-icon btn-default"><i class="fa fa-facebook-square"></i></a>
+                                    <a href="#pablo" class="btn btn-simple btn-just-icon btn-default"><i class="fa fa-facebook-square"></i></a> --}}
                                 </div>
                             </div>
                             <div class="col-md-4">
                                 <div class="team-player">
-                                    <img src="{{ asset('/MK_BS3/img/christian.jpg') }}" alt="Thumbnail Image" class="img-raised img-circle">
-                                    <h4 class="title">Christian Louboutin<br />
-                                        <small class="text-muted">Designer</small>
+                                    <img src="{{ asset('/img/PascualLemus.jpg') }}" alt="Thumbnail Image" class="img-raised img-circle">
+                                    <h4 class="title">Pascual Lemus<br />
+                                        <small class="text-muted">Developer</small>
                                     </h4>
-                                    <p class="description">You can write here details about one of your team members. You can give more details about what they do. Feel free to add some <a href="#">links</a> for people to be able to follow them outside the site.</p>
+                                    {{-- <p class="description">You can write here details about one of your team members. You can give more details about what they do. Feel free to add some <a href="#">links</a> for people to be able to follow them outside the site.</p>
                                     <a href="#pablo" class="btn btn-simple btn-just-icon"><i class="fa fa-twitter"></i></a>
-                                    <a href="#pablo" class="btn btn-simple btn-just-icon"><i class="fa fa-linkedin"></i></a>
+                                    <a href="#pablo" class="btn btn-simple btn-just-icon"><i class="fa fa-linkedin"></i></a> --}}
                                 </div>
                             </div>
                             <div class="col-md-4">
                                 <div class="team-player">
-                                    <img src="{{ asset('/MK_BS3/img/kendall.jpg') }}" alt="Thumbnail Image" class="img-raised img-circle">
-                                    <h4 class="title">Kendall Jenner<br />
-                                        <small class="text-muted">Model</small>
+                                    <img src="{{ asset('/img/SergioSojo.jpg') }}" alt="Thumbnail Image" class="img-raised img-circle">
+                                    <h4 class="title">Sergio Sojo<br />
+                                        <small class="text-muted">Developer</small>
                                     </h4>
-                                    <p>You can write here details about one of your team members. You can give more details about what they do. Feel free to add some <a href="#">links</a> for people to be able to follow them outside the site.</p>
+                                    {{-- <p>You can write here details about one of your team members. You can give more details about what they do. Feel free to add some <a href="#">links</a> for people to be able to follow them outside the site.</p>
                                     <a href="#pablo" class="btn btn-simple btn-just-icon"><i class="fa fa-google-plus"></i></a>
                                     <a href="#pablo" class="btn btn-simple btn-just-icon"><i class="fa fa-youtube-play"></i></a>
-                                    <a href="#pablo" class="btn btn-simple btn-just-icon btn-default"><i class="fa fa-twitter"></i></a>
+                                    <a href="#pablo" class="btn btn-simple btn-just-icon btn-default"><i class="fa fa-twitter"></i></a> --}}
                                 </div>
                             </div>
                         </div>
                     </div>
-
-                </div>
-
-
-                <div class="section landing-section">
-                    <div class="row">
-                        <div class="col-md-8 col-md-offset-2">
-                            <h2 class="text-center title">Work with us</h2>
-                            <h4 class="text-center description">Divide details about your product or agency work into parts. Write a few lines about each one and contact us about any further collaboration. We will responde get back to you in a couple of hours.</h4>
-                            <form class="contact-form">
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <div class="form-group label-floating">
-                                            <label class="control-label">Your Name</label>
-                                            <input type="email" class="form-control">
+                    <div class="section landing-section">
+                        <div class="row">
+                            <div class="col-md-8 col-md-offset-2">
+                                <h2 class="text-center title">Work with us</h2>
+                                <h4 class="text-center description">Divide details about your product or agency work into parts. Write a few lines about each one and contact us about any further collaboration. We will responde get back to you in a couple of hours.</h4>
+                                <form class="contact-form">
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <div class="form-group label-floating">
+                                                <label class="control-label">Your Name</label>
+                                                <input type="email" class="form-control">
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="form-group label-floating">
+                                                <label class="control-label">Your Email</label>
+                                                <input type="email" class="form-control">
+                                            </div>
                                         </div>
                                     </div>
-                                    <div class="col-md-6">
-                                        <div class="form-group label-floating">
-                                            <label class="control-label">Your Email</label>
-                                            <input type="email" class="form-control">
+
+                                    <div class="form-group label-floating">
+                                        <label class="control-label">Your Messge</label>
+                                        <textarea class="form-control" rows="4"></textarea>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-md-4 col-md-offset-4 text-center">
+                                            <button class="btn btn-primary btn-raised">
+                                                Send Message
+                                            </button>
                                         </div>
                                     </div>
-                                </div>
-
-                                <div class="form-group label-floating">
-                                    <label class="control-label">Your Messge</label>
-                                    <textarea class="form-control" rows="4"></textarea>
-                                </div>
-
-                                <div class="row">
-                                    <div class="col-md-4 col-md-offset-4 text-center">
-                                        <button class="btn btn-primary btn-raised">
-                                            Send Message
-                                        </button>
-                                    </div>
-                                </div>
-                            </form>
+                                </form>
+                            </div>
                         </div>
                     </div>
-
                 </div>
             </div>
 
